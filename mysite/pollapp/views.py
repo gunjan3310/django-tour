77d25f90c2ee87b3aca39738485e3da44d37d921
gunjan3310 <yadavgunjan3310@live.com>
2020-08-10 15:00:45 +0545

    html template rendering with its required context data implemented here.

--- mysite/pollapp/views.py
+++ mysite/pollapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse,Http404
+from .models import Question
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Well the view is this")
+    question_list = Question.objects.order_by('pub_date')
+    output = ""
+    for q in question_list:
+        output = ",".join([q.question_text for q in question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'question_list':question_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return HttpResponse("You're looking at question %s."%question.question_text)
+
+
+def results(request,question_id):
+    return HttpResponse("You're looking at the result of question %s."%question_id)
+
+
+def vote(request,question_id):
+    return HttpResponse("You're voting a question %s."%question_id)
