b0e58eadd02d1cf0a8bb5db89ae7a9316fb3a256
gunjan3310 <yadavgunjan3310@live.com>
2020-08-12 19:43:27 +0545

    functionality of generic views, specificly ListView and DetailView is used.

--- mysite/pollapp/views.py
+++ mysite/pollapp/views.py
@@ -4,35 +4,22 @@ from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'question_list'
 
-def index(request):
-    question_list = Question.objects.order_by('pub_date')
-    output = ""
-    for q in question_list:
-        output = ",".join([q.question_text for q in question_list])
-    template = loader.get_template('polls/index.html')
-    context = {
-        'question_list':question_list,
-    }
-    return HttpResponse(template.render(context,request))
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    try:
-        question = Question.objects.get(pk=question_id)
-
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-    return render(request,'polls/detail.html',{'question':question})
-
-
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{
-        'question':question
-    })
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
